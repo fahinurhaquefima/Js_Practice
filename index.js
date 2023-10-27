07a65bfe52d39bf260ec72e0bae11655c8578ee2
fahinurhaquefima <fahinurhaquef@gmail.com>
2023-10-27 20:24:46 +0600

    js basic structure

---
+++ index.js
@@ -0,0 +1,84 @@
+// console.log("Hello World") // Print string
+
+// var name="Fima";
+// console.log(name); //variable diclare
+
+// const pi=3.1416
+// console.log(pi); //constant diclare
+
+// --------DataType Check-------
+// let mynum=345;
+// console.log(typeof(mynum));
+
+// -----------DataType convert method---------
+// mynum= toString(mynum) //any type of data can change string
+// mynum= parseInt(mynum) //any type of data can change Intiger Number
+// mynum= parseFloat(mynum) //any type of data can change float Number
+
+// --------Number type data---------
+// var number =3.45445656;
+// console.log(number.toFixed(3)); //float ar por ar number count kore
+// console.log(number.toPrecision(6)); //total number theke count kore
+
+//------Boolean Data type------
+// console.log(Number(true)); //true hole 1 return kore
+// console.log(Number(false)); //false hole 0 return kore
+
+//--------Add concatenate strings----------
+// let name1 = "Fahinur";
+// let name2 = " Haque";
+// let name3 = " Fima";
+// let fullName= name1+name2+name3;
+
+// console.log(fullName); //simple concatenate
+// console.log(fullName + "safika"); //consol.log ar moddhe concatenate
+
+//another example
+// var firstNum=20;
+// var lastNum= 50;
+// console.log("First Number is=" + firstNum + " And Last Number is=" + lastNum); //muliple string concatenate 
+
+
+// ----------Library functions for string----------
+
+// let text = "I love Bangladesh";
+// console.log(text.slice(0,4)); //valuer 0 number index thke 4 number index ar age prjonto slice kore nei means kate nei
+// or, 
+// let arr=[13,23,45,62,21,]
+// console.log(arr.slice(2,4)); //array ar moddhe slice
+
+// console.log(text.charAt(7)); //valuer index number count
+// console.log(text.toUpperCase()); 
+// console.log(text.toLowerCase());
+// console.log(text.length); //nijer valuer cracter ar length count
+
+// let userText = prompt("Enter your name");
+// console.log(userText.length); //user ar valuer length count
+
+// let text1="Bangladesh "
+// let text2="is a beautiful country"
+// console.log(text1.concat(text2)); //library function ar moddhome concatenate
+
+// ---------Airthmatic Operator--------
+// let a=14;
+// let b=8;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// console.log(a++ ,a);
+// console.log(b-- , b);
+// console.log(++a);
+// console.log(--b);
+
+// -----------Assignment Operator---------
+// let x=6;
+// let y=7;
+// console.log(x+=y);
+// console.log(x-=y);
+// console.log(x*=y);
+// console.log(x/=y);
+// console.log(x%=y);
+// console.log(x**=y);
